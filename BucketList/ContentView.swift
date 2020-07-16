bd5510814a12260d45bd38cbae664502cb671c4a
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-07-16 11:22:51 +0100

    Writing data to the documents directory

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,32 +5,29 @@
 //  Created by Gary Watson on 16/07/2020.
 //  Copyright © 2020 Gary Watson. All rights reserved.
 //
-// Adding conformance to Comparable for custome types
+// Writing data to the documents directory
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-    
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister")
-        ].sorted()
-
-var body: some View {
-    List(users) { user in
-        Text("\(user.lastName), \(user.firstName)")
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
-}
+    var body: some View {
+        Text("Hello, World!")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = self.getDocumentsDirectory().appendingPathComponent("message.txt")
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
