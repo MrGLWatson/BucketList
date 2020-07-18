a3e8092a4ec8b9a15f1f85f31e2831d684a22cb1
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-07-18 12:11:33 +0100

    customising MKMapView annotations

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,22 +6,54 @@
 //  Copyright © 2020 Gary Watson. All rights reserved.
 //  Using TouchID and FaceID with SwiftUI
 
+import MapKit
 import LocalAuthentication
 import SwiftUI
 
 
 struct ContentView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
+
     @State private var isUnlocked = false
     var body: some View {
-        VStack {
-            if self.isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace,showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // create a new location
+                        let  newLocation = MKPointAnnotation()
+                        newLocation.title = "Example Location"
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                .clipShape(Circle())
+                    .padding(.trailing)
+                }
             }
-         }
-        .onAppear(perform: authenticate)
-
+        }
+        .alert(isPresented: $showingPlaceDetails) {
+            Alert(title: Text(selectedPlace?.title ?? "unknown"), message: Text(selectedPlace?.subtitle ?? "missing place information"), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+                //edit this place
+            })
+        }
     }
     func authenticate() {
         let context = LAContext()
